784226b89900847f2d9b7b941e73aea8d2a91848
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-10-31 12:48:25 +0300

    added global context

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { PageHOC } from '../components';
+import { useGlobalContext } from '../context';
+
 
 const Home = () => {
+  const { } = useGlobalContext();
   return (
     <div>
       <h1 className='text-white text-xl'>Home</h1>
